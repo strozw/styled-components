68deb5eef067a8e2b796a02b90e7b08cb285739a
strozw <sxtxrxoxzxw@gmail.com>
2020-09-13 12:45:47 +0900

    add FORCE_ENABLE_SPEEDY

--- packages/styled-components/src/constants.js
+++ packages/styled-components/src/constants.js
@@ -15,6 +15,7 @@ export const SPLITTER = '/*!sc*/\n';
 export const IS_BROWSER = typeof window !== 'undefined' && 'HTMLElement' in window;
 
 export const DISABLE_SPEEDY =
+  !process.env.FORCE_ENABLE_SPEEDY &&
   (typeof SC_DISABLE_SPEEDY === 'boolean' && SC_DISABLE_SPEEDY) ||
   (typeof process !== 'undefined' &&
     (process.env.REACT_APP_SC_DISABLE_SPEEDY || process.env.SC_DISABLE_SPEEDY)) ||
