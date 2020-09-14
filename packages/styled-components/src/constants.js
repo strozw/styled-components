b7778d12d885db0aabb293d1f190f37c1b065d68
strozw <sxtxrxoxzxw@gmail.com>
2020-09-14 11:39:13 +0900

    fix expression

--- packages/styled-components/src/constants.js
+++ packages/styled-components/src/constants.js
@@ -15,11 +15,12 @@ export const SPLITTER = '/*!sc*/\n';
 export const IS_BROWSER = typeof window !== 'undefined' && 'HTMLElement' in window;
 
 export const DISABLE_SPEEDY =
-  !process.env.FORCE_ENABLE_SPEEDY &&
-  (typeof SC_DISABLE_SPEEDY === 'boolean' && SC_DISABLE_SPEEDY) ||
-  (typeof process !== 'undefined' &&
-    (process.env.REACT_APP_SC_DISABLE_SPEEDY || process.env.SC_DISABLE_SPEEDY)) ||
-  process.env.NODE_ENV !== 'production';
+  !process.env.FORCE_ENABLE_SPEEDY && (
+    (typeof SC_DISABLE_SPEEDY === 'boolean' && SC_DISABLE_SPEEDY) ||
+    (typeof process !== 'undefined' &&
+      (process.env.REACT_APP_SC_DISABLE_SPEEDY || process.env.SC_DISABLE_SPEEDY)) ||
+    process.env.NODE_ENV !== 'production'
+  );
 
 // Shared empty execution context when generating static styles
 export const STATIC_EXECUTION_CONTEXT = {};
